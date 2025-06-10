d501c6aabf876ae4e9581ecc248269e0c750530e
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-06-10 15:32:58 +0500

    DateTime using day.js

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,40 +1,17 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { counterComponent } from "../../shared/counter/counter.component";
 import { RouterLink } from "@angular/router";
-import dayjs from "dayjs";
+import { DateTimeComponent } from "../../shared/date-time/date-time.component";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [counterComponent, RouterLink, MatButtonModule, MatIconModule],
+    imports: [RouterLink, counterComponent, DateTimeComponent, MatButtonModule, MatIconModule],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.scss",
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent {
     readonly title = "Homepage";
-    date: string = "";
-    time: string = "";
-    private intervalId: any;
-
-    constructor(private cdr: ChangeDetectorRef) {}
-
-    ngOnInit(): void {
-        this.updateTime();
-        this.intervalId = setInterval(() => {
-            this.updateTime();
-            this.cdr.markForCheck();
-        }, 1000);
-    }
-
-    updateTime(): void {
-        const now = dayjs();
-        this.date = now.format("ddd DD MMM YYYY");
-        this.time = now.format("hh:mm:ss A");
-    }
-
-    ngOnDestroy(): void {
-        clearInterval(this.intervalId);
-    }
 }
