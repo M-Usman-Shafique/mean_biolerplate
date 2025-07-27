a16bba599ab528ae50ba4a70b7a8a65b607c954a
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-07-27 21:20:27 +0500

    .env latest setup

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -1,20 +1,30 @@
-import dotenv from "dotenv";
-dotenv.config();
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 3000;
+const CLIENT_URL = process.env.CLIENT_URL;
+const API_URL = process.env.API_URL;
 
-export const NODE_ENV = process.env.NODE_ENV;
-export const PORT = process.env.PORT || 3000;
-export const CLIENT_URL = process.env.CLIENT_URL;
-export const API_URL = process.env.API_URL;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
 
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export const MONGODB_URI = process.env.MONGODB_URI;
-
-export const CLOUDINARY = {
+const CLOUDINARY = {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
+};
+
+export {
+    NODE_ENV,
+    PORT,
+    CLIENT_URL,
+    API_URL,
+    ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRY,
+    REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRY,
+    MONGODB_URI,
+    CLOUDINARY,
 };
